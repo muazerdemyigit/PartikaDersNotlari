e327b037a5558b4e782759e7743eceb0835da5ad
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-02-15 16:00:11 +0300

    Pratikler - If-ElseIf-TernaryIf Tamamlandı

---
+++ Başlangıç Seviyesi .Net Core Patikası/5) C# 101/Pratikler/6) If-ElseIf-Ternary-If/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _6__If_ElseIf_Ternary_If;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if (time >= 6 && time < 11)//eğer time 6 ile 11 arasındaysa buraya girer
+        {
+            Console.WriteLine("Günaydın");
+        }
+
+        else if (time >= 11 && time < 18)//eğer time 11 ile 18 arasındaysa buraya girer
+        {
+            Console.WriteLine("Tünaydın");
+        }
+        else//18den büyükse buraya girer
+        {
+            Console.WriteLine("İyi Geceler");
+        }
+
+        string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";//time 18 den küçük eşitse iyi günler, değilse iyi geceler yaz. ? ise, : değilse anlamına geliyor.
+
+        sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+    }
+}
