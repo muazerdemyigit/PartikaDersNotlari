f14565b099f276f7c332ef4d833afca0e9eb74c5
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-02-15 08:46:04 +0300

    Operatörler Bölümü Tamamlandı Ve Projeler Debug Edildi

---
+++ Başlangıç Seviyesi .Net Core Patikası/5) C# 101/Pratikler/3) Operatorler/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace _3__Operatorler;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //****************************
+        Console.WriteLine("***********İşlemli Atama Ve Atama Operatörleri**************");
+        //İşlemli Atama Ve Atama Operatörleri
+
+        int x = 3;
+        int y = 3;
+
+        y = y + 2;
+        Console.WriteLine(y);
+
+        y += 2;
+        Console.WriteLine(y);
+
+        y /= 1;
+        Console.WriteLine(y);
+
+        x *= 2;
+        Console.WriteLine(x);
+
+        //****************************
+
+
+        //****************************
+        Console.WriteLine("***********Mantıksal Operatörler**************");
+        //Mantıksal Operatörler
+
+        // ||(veya), &&(ve), !(değil)
+
+        bool isSucces = true;
+        bool isCompleted = false;
+
+        if (isSucces && isCompleted)
+        {
+            Console.WriteLine("Perfect");
+        }
+
+        if (isSucces || isCompleted)
+        {
+            Console.WriteLine("Great");
+        }
+
+        if (isSucces && !isCompleted)
+        {
+            Console.WriteLine("Fine");
+        }
+
+        //****************************
+
+
+        //****************************
+        Console.WriteLine("***********İlişkisel Operatörler**************");
+        //İlişkisel Operatörler
+
+        // <(küçüktür), >(büyüktür), <=(küçük eşittir), >=(büyük eşittir), ==(eşittir), !=(eşit değildir)
+
+        int a = 3;
+        int b = 4;
+        bool sonuc = a < b;
+        Console.WriteLine(sonuc);
+
+        sonuc = a > b;
+        Console.WriteLine(sonuc);
+
+        sonuc = a >= b;
+        Console.WriteLine(sonuc);
+
+        sonuc = a <= b;
+        Console.WriteLine(sonuc);
+
+        sonuc = a == b;
+        Console.WriteLine(sonuc);
+
+        sonuc = a != b;
+        Console.WriteLine(sonuc);
+        //****************************
+
+
+        //****************************
+        Console.WriteLine("***********Aritmetik Operatörler**************");
+        //Aritmetik Operatörler
+
+        // /(bölü), *(çarpı), +(artı), -(eksi), %(mod alma)
+
+        int sayi1 = 10;
+        int sayi2 = 5;
+
+        int sonuc1 = sayi1 / sayi2;
+        Console.WriteLine(sonuc1);
+
+        sonuc1 = sayi1 * sayi2;
+        Console.WriteLine(sonuc1);
+
+        sonuc1 = sayi1 + sayi2;
+        Console.WriteLine(sonuc1);
+
+        sonuc1 = sayi1 - sayi2;
+        Console.WriteLine(sonuc1);
+
+        int sonuc2 = 20 % 3;
+        Console.WriteLine(sonuc2);
+
+
+        //****************************
+
+    }
+}
+
