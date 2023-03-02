41ee99f9689a6fc4ab16d9a764e273ba6f8059e4
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-03-02 07:58:33 +0300

    Karakter Değiştirme Projesi Tamamlandı

---
+++ Başlangıç Seviyesi .Net Core Patikası/7) C# Projeleri/2) Orta Seviye Projeler/4) Karakter Değiştirme/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _4__Karakter_Değiştirme;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Algoritma
+            Verilen string ifade içerisindeki ilk ve son karakterin yerini değiştirip tekrar ekrana yazdıran console uygulamasını yazınız.
+
+            Örnek: Input: Merhaba Hello Algoritma x
+
+            Output: aerhabM oellH algoritmA x
+        */
+        Console.Write("Bir metin giriniz: ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+        string[] newWords = new string[words.Length];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length <= 1)
+            {
+                newWords[i] = words[i];
+            }
+            else
+            {
+                char firstChar = words[i][0];
+                char lastChar = words[i][words[i].Length - 1];
+
+                string newWord = lastChar + words[i].Substring(1, words[i].Length - 2) + firstChar;
+                newWords[i] = newWord;
+            }
+        }
+
+        string output = string.Join(" ", newWords);
+        Console.WriteLine($"Yeni metin: {output}");
+    }
+}
