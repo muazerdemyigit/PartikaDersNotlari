742ba44abffda8505d673700da4088389bdbf08b
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-03-02 09:06:24 +0300

    Sessiz Harf Projesi Tamamlandı

---
+++ Başlangıç Seviyesi .Net Core Patikası/7) C# Projeleri/2) Orta Seviye Projeler/5) Sessiz Harf/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _5__Sessiz_Harf;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Algoritma
+            Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasını yazınız.
+
+            Örnek: Input: Merhaba Umut Arya
+
+            Output: True False True
+        */
+        System.Console.WriteLine("Lütfen bir metin giriniz:");
+        string metin = Console.ReadLine();
+
+        string[] kelimeler = metin.Split(' ');
+        string unsuzler = "bcdfghjklmnpqrstvwxyz";
+
+        foreach (var item in kelimeler)
+        {
+            bool ardisikUnsuz = false;
+            for (int i = 0; i < item.Length - 1; i++)
+            {
+                if (unsuzler.Contains(item[i]) && unsuzler.Contains(item[i + 1]))
+                {
+                    ardisikUnsuz = true;
+                    break;
+                }
+            }
+            System.Console.WriteLine(ardisikUnsuz + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
