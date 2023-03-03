df631071ade6b33b79b50153ce289620865788ea
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-03-03 11:25:08 +0300

    js Dosayaları Eklendi

---
+++ Başlangıç Seviyesi .Net Core Patikası/7) C# Projeleri/3) Zor Seviye Projeler/1) Voting Uygulaması/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1__Voting_Uygulaması;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Voting Uygulaması
+        
+            Uygulama çalıştığında pre-defined olarak belirlenen kategoriler oylamaya sunulmak üzere listelenmelidir. Yalnızca sisemde kayırlı olan kullanıcılar oy verebilir. Oylama sırasında öncelikle kullanıcının username'i istenmelidir. Eğer sistemde kayıtlı değilse kayıt olmasına imkan sağlanmalı ve kaldığı yerden oylamaya devam edebilmelidir. Kategoriler isteğe bağlı olarak belirlenebilir.
+
+            Bazı Örnek Kategoriler: Film Kategorileri Tech Stack Kategorileri Spor Kategorileri
+
+            Son olarak uygulama sonlandırılırken, Voting sonuçları hem rakamsal hem de yüzdesel olarak gösterilmelidir.
+
+            Kullanılması gereken teknikler:
+
+            - Kategoriler pre-defined kullanılabilir.
+        */
+    }
+}
